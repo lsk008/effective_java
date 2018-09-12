1b48b9d2ff5d27880da97a4d0e861df5145bde7d
lsk008 <lsk008@live.cn>
2018-09-13 00:44:27 +0800

    !

---
+++ src/main/java/com/lsk/translate/java/effective/item1/Student.java
@@ -0,0 +1,31 @@
+package com.lsk.translate.java.effective.item1;
+
+public class Student {
+    private String name;
+    private int age;
+    //constructor
+    public Student(){
+
+    }
+
+    public Student(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    public Student(int age,String name){
+        this.name=name;
+        this.age=age;
+    }
+    //static factory method
+    public Student createInstance(){
+        return new Student();
+    }
+    public static void main(String[] args) {
+        //use constructor to create Student instance
+        Student student1=new Student();
+        Student student2=new Student("student2",18);
+    }
+    public static Boolean valueOf(boolean b){
+        return b?Boolean.TRUE:Boolean.FALSE;
+    }
+}
